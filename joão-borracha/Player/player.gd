378b933c582e56f0a8c7a33580825cc21f5254d6
Filipe Santos <184993637+PH1LIPPUS@users.noreply.github.com>
2025-03-20 12:15:14 +0000

    Revert "Merge branch 'main' of https://github.com/PH1LIPPUS/JoaoBorracha"
    This reverts commit 28e0837bc61224020703ca9890774082946b5af0, reversing
    changes made to 95e92d011535de15765108686dbb8196179d0fe4.

--- joão-borracha/Player/player.gd
+++ joão-borracha/Player/player.gd
@@ -5,11 +5,13 @@ extends CharacterBody2D
 @export var jump_force: float = 400.0
 @export var gravity: float = 900.0
 @export var max_health: int = 100
+@export var max_stamina: int = 100
+@export var stamina_cost: float = 10.0
 
 var health: int = max_health
+var stamina: int = max_stamina
 
 @onready var hands: Marker2D = $Hands
-@onready var sprite: Sprite2D = $Sprite2D  
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -18,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("left", "right")
 	var current_speed = speed
 
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and use_stamina(stamina_cost * delta):
 		current_speed = run_speed
 
 	velocity.x = direction * current_speed
@@ -29,22 +31,13 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+	regenerate_stamina(delta)
+
 func _process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var direction = (mouse_position - global_position).normalized()
-
-	hands.rotation = direction.angle()
-
 	var max_distance = 30.0  
 	hands.position = direction * max_distance
-
-	if mouse_position.x > global_position.x:
-
-		sprite.flip_h = false  
-		hands.flip_h = false 
-	else:
-		sprite.flip_h = true 
-		hands.flip_h = false 
 
 func take_damage(amount: int) -> void:
 	health -= amount
@@ -53,3 +46,13 @@ func take_damage(amount: int) -> void:
 
 func die() -> void:
 	print("Player morreu!")
+
+func use_stamina(amount: int) -> bool:
+	if stamina >= amount:
+		stamina -= amount
+		return true
+	return false
+
+func regenerate_stamina(delta: float) -> void:
+	if stamina < max_stamina:
+		stamina += 10 * delta
