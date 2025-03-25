77419b997d81c279e19719544602c9ca054e7216
Fred <a31702@aemtg.pt>
2025-03-25 09:35:27 +0000

    impact

--- joão-borracha/Scripts/cutseen.gd
+++ joão-borracha/Scripts/cutseen.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var label = $Label
+
+func _ready():
+	var tween = create_tween()
+	tween.tween_property(label, "modulate:a", 0.0, 20.0)
+	await get_tree().create_timer(20.0).timeout
+	get_tree().change_scene_to_file("res://Scene/game.tscn")
 
 func _on_skip_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scene/game.tscn")
