c84e10651b71881d6d2dd650f7afb3d9523c62fe
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-03-26 11:22:03 +0000

    jao2

---
+++ joaoborracha/Resources/Capsule/capsula.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var vida_restaurada: int = 1  
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") or body.name == "Player":  
+		if body.has_method("receber_cura"):
+			body.receber_cura(vida_restaurada)
+			queue_free()
