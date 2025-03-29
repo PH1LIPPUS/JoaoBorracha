901ae9cf4ee13e8dc6bc53bf6d03740d93a2abe1
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-03-29 11:59:36 +0000

    save

--- joaoborracha/Resources/Guns/Bullets/bullet.gd
+++ joaoborracha/Resources/Guns/Bullets/bullet.gd
@@ -3,12 +3,24 @@ extends CharacterBody2D
 var pos:Vector2
 var rota:float
 var dir: float
-var speed= 200
+var speed= 500
+var lifetime = 2.0
 
 func _ready():
 	global_position=pos
 	global_rotation=rota
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = lifetime
+	timer.one_shot = true
+	timer.timeout.connect(Callable(self, "queue_free"))
+	timer.start()
 	
 func _physics_process(delta):
 	velocity=Vector2(speed, 0).rotated(dir)
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	
+		queue_free()
