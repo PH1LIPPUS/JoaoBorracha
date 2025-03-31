d9c4f7fcedce99270769f204e6c6f2687b2cefb3
Filipe3110 <filipe.p.nunes2006@gmail.com>
2025-03-31 11:17:39 +0100

    saco

--- joaoborracha/Resources/Guns/Bullets/bullet.gd
+++ joaoborracha/Resources/Guns/Bullets/bullet.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
-var pos:Vector2
-var rota:float
+var pos: Vector2
+var rota: float
 var dir: float
-var speed= 500
-var lifetime = 2.0
+var speed = 500
+var lifetime = 4.0
+var damage = 10
 
 func _ready():
-	global_position=pos
-	global_rotation=rota
+	global_position = pos
+	global_rotation = rota
+	
+	# Setup bullet cleanup timer
 	var timer = Timer.new()
 	add_child(timer)
 	timer.wait_time = lifetime
@@ -17,10 +20,29 @@ func _ready():
 	timer.start()
 	
 func _physics_process(delta):
-	velocity=Vector2(speed, 0).rotated(dir)
-	move_and_slide()
+	# Make sure direction is properly set from rotation
+	velocity = Vector2(speed, 0).rotated(rota)
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		handle_collision(collision)
 
+func handle_collision(collision):
+	var collider = collision.get_collider()
+	
+	# Check if collider has health or is damageable
+	if collider.has_method("take_damage"):
+		collider.take_damage(damage)
+	
+	# Spawn impact effect if needed
+	# spawn_impact_effect(collision.get_position())
+	
+	# Remove the bullet
+	queue_free()
 
-func _on_area_2d_body_entered(body):
-		print("a")
-		queue_free()
+# Properly named collision function
+func _on_area_entered(area):
+	queue_free()
+
+func _on_body_entered(body):
+	queue_free()
--- joaoborracha/Resources/punching_bag.gd
+++ joaoborracha/Resources/punching_bag.gd
@@ -1,13 +1,9 @@
 extends CharacterBody2D
 
-var vida_atual = 10 
-var vida_maxima = 10  
-var dano = 1  # You need to define how much damage is applied
-@onready var text = $Label  # Use @onready to ensure the node is available when ready
+
+@onready var text =  $BagLabel
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	vida_atual -= dano
-	vida_atual = clamp(vida_atual, 0, vida_maxima)  
-	text.text = str("-10hp")  # Update the label text to show current health
-	if vida_atual <= 0:
-		queue_free()
+	text.text = "-10 hp"
+	await get_tree().create_timer(0.2).timeout 
+	text.text = ""
